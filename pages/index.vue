f858714f1cf447f2a9a994c90ea13fd92fb659b9
root <root@makkePC.localdomain>
2019-09-28 17:44:19 +0900

    [s

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+asyncData ({ env }) {
+  return client.getEntries({
+    'content_type': env.CTF_BLOG_POST_TYPE_ID,
+    order: '-fields.publishDate',
+    'limit': 3
+  }).then(entries => {
+    return {
+      posts: entries.items
+    }
+  }).catch(console.error)
+}
